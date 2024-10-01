32c3f8683b8030b42d41d44f76f24ab59303e406
ErildoJS <erildofranciscojs@gmail.com>
2024-10-01 13:31:11 +0100

    fix: indent all component

--- app/_components/booking-summary.tsx
+++ app/_components/booking-summary.tsx
@@ -1,39 +1,39 @@
-import { Card, CardContent } from "./ui/card";
+// import { Card, CardContent } from "./ui/card";
 
-export function BookingSummary() {
-  return (
-    <Card>
-      <CardContent className="p-3 space-y-3">
-        <div className="flex items-center justify-between">
-          <h2 className="font-bold">{service.name}</h2>
-          <p className="text-sm font-bold">
-            {Intl.NumberFormat("pt-AO", {
-              style: "currency",
-              currency: "KWZ",
-            }).format(Number(service.price))}
-          </p>
-        </div>
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-gray-400">Data</h2>
-          <p className="text-sm">
-            {format(selectedDay, "d 'de' MMMM", {
-              locale: pt
-            })}
-          </p>
-        </div>
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-gray-400">Horário</h2>
-          <p className="text-sm">
-            {selectedTime}
-          </p>
-        </div>
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-gray-400">Barbearia</h2>
-          <p className="text-sm">
-            {barbershop.name}
-          </p>
-        </div>
-      </CardContent>
-    </Card>
-  )
-}
+// export function BookingSummary() {
+//   return (
+//     <Card>
+//       <CardContent className="p-3 space-y-3">
+//         <div className="flex items-center justify-between">
+//           <h2 className="font-bold">{service.name}</h2>
+//           <p className="text-sm font-bold">
+//             {Intl.NumberFormat("pt-AO", {
+//               style: "currency",
+//               currency: "KWZ",
+//             }).format(Number(service.price))}
+//           </p>
+//         </div>
+//         <div className="flex items-center justify-between">
+//           <h2 className="text-sm text-gray-400">Data</h2>
+//           <p className="text-sm">
+//             {format(selectedDay, "d 'de' MMMM", {
+//               locale: pt
+//             })}
+//           </p>
+//         </div>
+//         <div className="flex items-center justify-between">
+//           <h2 className="text-sm text-gray-400">Horário</h2>
+//           <p className="text-sm">
+//             {selectedTime}
+//           </p>
+//         </div>
+//         <div className="flex items-center justify-between">
+//           <h2 className="text-sm text-gray-400">Barbearia</h2>
+//           <p className="text-sm">
+//             {barbershop.name}
+//           </p>
+//         </div>
+//       </CardContent>
+//     </Card>
+//   )
+// }
